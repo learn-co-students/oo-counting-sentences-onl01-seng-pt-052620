a40c23d01dfcdeffa30bf35ac842b06740759765
Sean Bordelon <sean.j.bordelon@gmail.com>
2020-06-12 03:34:58 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,32 @@
-require 'pry'
+#require "pry"
 
 class String
 
   def sentence?
+
+    self.match?(/^.*[.]$/)
     
   end
 
   def question?
 
+    self.end_with?("?")
+
   end
 
   def exclamation?
 
+    self.match?(/^.*[!]$/)
+
   end
 
   def count_sentences
+    
+    
+    @array = self.split(/[.?!]+/)
+    puts @array
+    @array.count
+
 
   end
 end
